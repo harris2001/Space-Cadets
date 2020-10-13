0d4a22ba1bdbeb0b3d89116f74684eb6b536491c
hacker2001 <harrishadjiantonis@gmail.com>
2020-10-13 17:28:30 +0300

    Add files via upload

--- Space Cadets/src/exercise3/BareBonesExtended.java
+++ Space Cadets/src/exercise3/BareBonesExtended.java
@@ -1,21 +1,22 @@
-package exercise2;
+package exercise3;
 import java.io.*;
 import java.util.*;
 
 
-public class BareBones {
+public class BareBonesExtended {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-
-		File file_obj = new File("bone.txt");
+		//reading file
+		File file_obj = new File("bone2.txt");
 		try {
 			Scanner sc = new Scanner(file_obj);
-			BareBones bb = new BareBones();
+			BareBonesExtended bb = new BareBonesExtended();
 			bb.Interpreter(sc);
 		}
+		//if file doesn't exist, throw exeption
 		catch (FileNotFoundException e) {
-			//System.out.println("An error occured while reading file");
+			System.out.println("An error occured while reading file");
 			e.printStackTrace();
 		}
 	}
@@ -27,20 +28,24 @@ public class BareBones {
 		Vector<Integer>s_pointers = new Vector<Integer>();
 		
 		variable var;
-		////System.out.println("ok");
 
 		//We begin with a large loop (while ~ == 1)
+		
 		//null variable
 		var = new variable();
 		var.name="!";
 		variables.add(var);
+		
 		//~ = 1
 		var = new variable();
 		var.name="~";
 		var.value=1;
 		variables.add(var);
 		counters.add("~");
+		//starting from instruction at position 0
 		s_pointers.add(0);
+		
+		//while ~= 1 (repeat every instruction in the file once)
 		String[] starting = new String[2];
 		starting[0]="while";
 		starting[1]="~";
@@ -48,19 +53,14 @@ public class BareBones {
 		
 		while(sc.hasNextLine()) {
 			String[] instr = sc.nextLine().trim().split("\\s");
-			if(instr[0].equals("end;")) {
+			if(instr[0].equals("end")) {
 				instr = new String[2];
 				instr[0]="end";
 				instr[1] = ";";
 
 			}
-			else {
-				instr[1]=instr[1].replace(";","");
-				////System.out.println("Storing: "+instr[0]+" "+instr[1]);
-			}
 			instructions.add(instr);
 		}
-		
 		String[] ending_instr = new String[2];
 		ending_instr[0] = "end";
 		ending_instr[1] = ";";
@@ -68,40 +68,44 @@ public class BareBones {
 		
 		return_type ret= new return_type();
 		ret = Compile(variables,instructions,counters,s_pointers);
+		System.out.println("oo");
 		print_results(ret.variables);
 	}
 
 	return_type Compile(Vector<variable> variables, Vector<String[]> instructions,Vector<String> counters,Vector<Integer>s_pointers) {
-
-		int esp=s_pointers.lastElement();
-		int ebp = esp;
 		String[] instr;
-		////System.out.println(">>>>>>>"+instructions.get(esp)[0]+"<>"+instructions.get(esp)[1]);
-		while(Get_variable(variables,instructions.get(ebp)[1]).value>0) {
-			//System.out.println("Executing "+Get_variable(variables,instructions.get(ebp)[1]).value+"th time");
+		//print_results(variables);
+		int esp=s_pointers.lastElement();
+		System.out.println(">>>>>>>"+instructions.get(esp)[0]+" "+instructions.get(esp)[1]);
+		System.out.println("Executing "+Get_variable(variables,instructions.get(esp)[1]).value+" times");
+		int ebp;
+		int r_esp=esp;
+		while(Get_variable(variables,instructions.get(esp)[1]).value>0) {
+			ebp = esp;
+			esp++;
 			do{
-				esp++;
-				//System.out.println("esp:"+esp);
 				instr=instructions.get(esp);
+				System.out.println(instr[0]);
 				//System.out.println(instr[0]+" "+instr[1]);
 				if(instr[0].equals("while")) {
 					s_pointers.add(esp);
 					//System.out.println("adding pointer: "+(esp));
 					counters.add(instr[1]);
 					return_type ret = new return_type();
-					
-					//updating vectors after loop the loop is over
 					ret = Compile(variables,instructions,counters,s_pointers);
 					variables = ret.variables;
 					instructions = ret.instructions;
 					counters = ret.counters;
 					esp = ret.esp;
+					s_pointers = s_pointers;
+					s_pointers.remove(s_pointers.lastElement());
+					//System.out.println(s_pointers.lastElement()+" "+esp);
 				}
 				if(instr[0].equals("clear")) {
-					variable vv = Get_variable(variables,instr[1]);
+					variable vv = Get_variable(variables,instr[1].replace(";", ""));
 					if(vv.name=="!") {
 						variable v = new variable();
-						v.name=instr[1];
+						v.name=instr[1].replace(";", "");
 						v.value=0;
 						variables.add(v);
 					}
@@ -116,32 +120,24 @@ public class BareBones {
 					Get_variable(variables,instr[1]).value--;
 				}
 				if(instr[0].equals("print")) {
-					//System.out.println(Get_variable(variables,instr[1]).value);
+					System.out.println(Get_variable(variables,instr[1]).value);
 				}
+				esp++;
 			}while(!instr[0].equals("end"));
-			//System.out.println("###"+Get_variable(variables,instructions.get(ebp)[1]).value);//Get_variable(variables,instructions.get(ebp)[1]).name);
 			if(Get_variable(variables,counters.get(1)).value==0) {
 				//System.out.println("EXITING");
 				break;
 			}
-			if(Get_variable(variables,instructions.get(ebp)[1]).value>0){
-				esp=s_pointers.lastElement();
-				//System.out.println("repeating from s_pointer: "+esp);
-			}
-			else {
-				//System.out.println("Removing s_pointer");
-				s_pointers.remove(s_pointers.lastElement());
-				//System.out.println("continue"+esp);
-				return_type ret = new return_type();
-				ret.init(variables,instructions,counters,s_pointers,esp);
-
-				return ret;
-			}
+			r_esp = esp;
+			esp=ebp;
 		}
-		
 		return_type ret = new return_type();
-		ret.init(variables,instructions,counters,s_pointers,esp);
+		ret.init(variables,instructions,counters,s_pointers,r_esp);
 
+		/*System.out.println("Returning:");
+		System.out.println("____________________________");*/
+		
+		//print_results(variables);
 		return ret;
 	}
 	
@@ -149,7 +145,7 @@ public class BareBones {
 		for(int i=2; i<variables.size(); i++) {
 			String name = variables.get(i).name;
 			Integer value = variables.get(i).value;
-			System.out.println(name+" whith value "+value);
+			System.out.println(name.replace(";","")+" whith value "+value);
 		}
 	}
 	
@@ -182,6 +178,6 @@ class return_type{
 		this.instructions = instructions;
 		this.counters = counters;
 		this.s_pointers = s_pointers;
-		this.esp = esp;
+		this.esp = esp-1;
 	}
 }
